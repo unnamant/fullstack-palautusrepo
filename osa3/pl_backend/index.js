6f668e09fddd42f1a2f812655782d371ec51118f
Unna Mäntyniemi <unna.mantyniemi@helsinki.fi>
2026-01-23 12:53:56 +0200

    uibuild

--- osa3/pl_backend/index.js
+++ osa3/pl_backend/index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
 
-app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('body', (request) => {
   if (request.method === 'POST') {
